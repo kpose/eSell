ee0c4f5163e20e9c2717251acc7dfa0acb51639c
kpose <ganihujude@gmail.com>
2021-11-09 21:29:03 +0100

    starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {
+  NavigationContainer,
+  DefaultTheme as NavigationDefaultTheme,
+  DarkTheme as NavigationDarkTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+
+const CombinedDefaultTheme = {
+  ...PaperDefaultTheme,
+  ...NavigationDefaultTheme,
+};
+const CombinedDarkTheme = {...PaperDarkTheme, ...NavigationDarkTheme};
 
 const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = React.useState(true);
+
+  const theme = isDarkTheme ? CombinedDarkTheme : CombinedDefaultTheme; // Use Light/Dark theme based on a state
+
+  function toggleTheme() {
+    // We will pass this function to Drawer and invoke it on theme switch press
+    setIsDarkTheme(isDark => !isDark);
+  }
   return (
-    <View>
-      <Text>hello, welome</Text>
-    </View>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <Text>hello, welome</Text>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
